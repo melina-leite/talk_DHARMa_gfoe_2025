70ebe177901a7c4b84707275ad077d6d8a62182a
melina-leite <melina.leite@ib.usp.br>
2025-09-01 09:10:32 +0200

    final version for conference

--- figures_dispersion_problems.R
+++ figures_dispersion_problems.R
@@ -1,6 +1,8 @@
 #devtools::install_github("gadenbuie/ggpomological")
 
-
+library(tidyverse)
+library(ggeffects)
+library(glmmTMB)
 library(ggpomological)
 library(magick)
 
@@ -161,6 +163,7 @@ ggsave(test,filename="images/plot_zeroinflation.png",
 
 ## CONSEQUENCES FIGURE ----
 
+### OVERDISPERSION ----
 set.seed(5) 
 heteroData = createData(sampleSize = 100, intercept = 1,  
                         overdispersion = 1, 
@@ -174,7 +177,7 @@ predHetero <- MASS::glm.nb(observedResponse ~ Environment1,
                       data=heteroData) |>
   ggpredict(terms="Environment1") |> 
   as.data.frame()
-preds <- bind_rows(list(Poisson = predPois, `negative binomial` = predHetero),
+preds <- bind_rows(list(`Poisson (Wrong)` = predPois, `Negative Binomial (Correct)` = predHetero),
                    .id="model")
 #"#c03728" "#919c4c"
 
@@ -198,11 +201,64 @@ ggplot(heteroData, aes(x=Environment1, y=observedResponse))+
         legend.text = element_text(size=20),
         legend.title = element_text(size=20),
         legend.position = "inside",
-        legend.position.inside = c(0.3,0.8)) -> test
+        legend.position.inside = c(0.4,0.8)) -> test
 
-ggsave(test,filename="images/plot_consequence_IC.png",
+ggsave(test,filename="images/plot_overdispersion_IC.png",
        width=6, heigh=6)
 #paint_pomological(test, res = 110, width=600, height=600) %>%
 #  magick::image_write("images/plot_heteroscedasticity.png")
 
 
+
+
+
+
+
+
+### UNDERDISPERSION ---
+
+set.seed(123)
+x <- runif(100, -1, 1)
+mu <- exp(1 + 0.7 * x)
+
+y_under <- rbinom(100, size = , prob = mu / 12)
+
+dataU <- data.frame(Abundance = y_under, Environment = x)
+
+
+predPois <- glm(Abundance ~ Environment, data=dataU,
+                family=poisson()) |>
+  ggpredict(terms="Environment") |> as.data.frame()
+predHetero <- glmmTMB(Abundance ~ Environment,
+                           data=dataU, family = compois()) |>
+  ggpredict(terms="Environment") |> 
+  as.data.frame()
+preds <- bind_rows(list(Wrong= predPois, Correct = predHetero),
+                   .id="model") %>%
+  mutate(model = fct_relevel(model,"Wrong", "Correct"))
+preds$conf.low[1:11] <- preds$conf.low[1:11] - 0.4*seq(0,1,length.out=11)
+preds$conf.high[1:11] <- preds$conf.high[1:11] + 0.4*seq(0,1,length.out=11)
+
+ggplot(dataU, aes(x=Environment, y=Abundance))+
+  geom_point(alpha=0.1, size=4) + 
+  geom_line(data=preds, aes(x=x, y=predicted, col=model), linewidth=2) +
+  geom_ribbon(data=preds, aes(x=x, y=predicted, fill=model, ymin=conf.low,
+                              ymax=conf.high), col=NA, alpha=0.3) +
+  xlab("Enviroment") + ylab("Abundance") +
+  scale_color_manual(values =c( "#f52300","#008B00"))+
+  scale_fill_manual(values =c( "#f52300","#008B00"))+
+  theme_pomological_plain()+
+ # ylim(0,22)+
+  ggtitle("Underdispersed data")+
+  theme(axis.text = element_text(size=20),
+        # axis.text.x = element_blank(),
+        title = element_text(size=25),
+        axis.title = element_text(size=20),
+        panel.background = element_rect(linewidth = 3, fill = NA),
+        legend.text = element_text(size=20),
+        legend.title = element_text(size=20),
+        legend.position = "inside",
+        legend.position.inside = c(0.3,0.8)) -> test
+
+ggsave(test,filename="images/plot_underdispersion_IC.png",
+       width=6, heigh=6)
